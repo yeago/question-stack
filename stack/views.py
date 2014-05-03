e19c62e9971114170acff7ea32b8163ec9272003
yeago <subsume@gmail.com>
2014-05-03 21:24:36 +0200

    Add site

--- stack/views.py
+++ stack/views.py
@@ -5,6 +5,7 @@ from django.http import Http404
 from django.views.generic.list import ListView
 from django.shortcuts import render_to_response, redirect, get_object_or_404
 
+from django.conf import settings
 from django.contrib.auth.models import User
 
 from django.contrib import messages
@@ -17,66 +18,66 @@ from stack import models as sm
 from stack import forms as sf
 
 def add(request,form_class=sf.QuestionForm):
-	form = form_class(request.POST or None,prefix="question")
-	if request.POST and form.is_valid():
-		instance = form.save()
-		CommentModel = comments.get_model()
-		user = request.user
-		if form.cleaned_data.get('username'):
-			user = User.objects.get(username=form.cleaned_data.get('username'))
+    form = form_class(request.POST or None,prefix="question")
+    if request.POST and form.is_valid():
+        instance = form.save()
+        CommentModel = comments.get_model()
+        user = request.user
+        if form.cleaned_data.get('username'):
+            user = User.objects.get(username=form.cleaned_data.get('username'))
 
-		ct = ContentType.objects.get_for_model(instance)
-		instance.comment = CommentModel.objects.create(\
-			comment=form.cleaned_data.get('comment'),
-			user=user,
-			content_type=ct,
-			object_pk=instance.pk,
-			site=Site.objects.get_current(),
-			submit_date=form.cleaned_data.get('date',None) or datetime.now())
+        ct = ContentType.objects.get_for_model(instance)
+        instance.comment = CommentModel.objects.create(\
+            comment=form.cleaned_data.get('comment'),
+            user=user,
+            content_type=ct,
+            object_pk=instance.pk,
+            site=Site.objects.get_current(),
+            submit_date=form.cleaned_data.get('date',None) or datetime.now())
 
-		instance.save()
-		messages.success(request,"Question posted")
-		return redirect(instance.get_absolute_url())
+        instance.save()
+        messages.success(request,"Question posted")
+        return redirect(instance.get_absolute_url())
 
-	return render_to_response("stack/add.html",{'form': form, },\
-		context_instance=RequestContext(request))
+    return render_to_response("stack/add.html",{'form': form, },\
+        context_instance=RequestContext(request))
 
 def home(request):
-	return render_to_response("stack/home.html", {"answered": sm.Question.objects.filter(\
-		has_answer=True).order_by('-pk'),\
-		"unanswered": sm.Question.objects.filter(has_answer=False)},context_instance=RequestContext(request))
+    return render_to_response("stack/home.html", {"answered": sm.Question.objects.filter(site=settings.SITE_ID,\
+        has_answer=True).order_by('-pk'),\
+        "unanswered": sm.Question.objects.filter(site=settings.SITE_ID, has_answer=False)},context_instance=RequestContext(request))
 
 
 class QuestionList(ListView):
     paginate_by = 20
     def get_queryset(self, **kwargs):
-        return sm.Question.objects.select_related(\
-		    'accepted_answer','comment').order_by('-comment__submit_date')
+        return sm.Question.objects.filter(site=settings.SITE_ID).select_related(\
+            'accepted_answer','comment').order_by('-comment__submit_date')
 
 def detail(request,slug):
-	instance = get_object_or_404(sm.Question,slug=slug)
-	CommentModel = comments.get_model()
-	responses = CommentModel.objects.for_model(instance).exclude(pk=instance.comment_id)
-	return render_to_response("stack/question_detail.html",{'instance': instance, 'responses': responses},\
-		context_instance=RequestContext(request))
+    instance = get_object_or_404(sm.Question,slug=slug, site=settings.SITE_ID)
+    CommentModel = comments.get_model()
+    responses = CommentModel.objects.for_model(instance).exclude(pk=instance.comment_id)
+    return render_to_response("stack/question_detail.html",{'instance': instance, 'responses': responses},\
+        context_instance=RequestContext(request))
 
 def accepted_answer(request,slug,comment):
-	instance = get_object_or_404(sm.Question,slug=slug)
+    instance = get_object_or_404(sm.Question,slug=slug, site=settings.SITE_ID)
 
-	Comment = comments.get_model()
-	comment = get_object_or_404(Comment,pk=comment)
-	if not instance == comment.content_object:
-		raise Http404
+    Comment = comments.get_model()
+    comment = get_object_or_404(Comment,pk=comment)
+    if not instance == comment.content_object:
+        raise Http404
 
-	if not request.user.has_perm('stack.change_question') and not request.user == instance.comment.user:
-		raise Http404
+    if not request.user.has_perm('stack.change_question') and not request.user == instance.comment.user:
+        raise Http404
 
-	if instance.accepted_answer:
-		messages.warning(request,"You have changed the accepted answer")
-	else:
-		messages.success(request,"Answer has been marked as accepted")
+    if instance.accepted_answer:
+        messages.warning(request,"You have changed the accepted answer")
+    else:
+        messages.success(request,"Answer has been marked as accepted")
 
-	instance.accepted_answer = comment
-	instance.has_answer = True
-	instance.save()
-	return redirect(instance.get_absolute_url())
+    instance.accepted_answer = comment
+    instance.has_answer = True
+    instance.save()
+    return redirect(instance.get_absolute_url())
