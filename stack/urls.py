24c5c12332beeb95cb9fcf48159d77deac573f87
ivaggione <ivaggione@gmail.com>
2023-02-14 23:38:44 +0000

    Django 4

--- stack/urls.py
+++ stack/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import re_path
 from stack import views
 
 urlpatterns = [
-    url('^$', views.home, name="stack_question_home"),
-    url('^all/$', views.QuestionList.as_view(), name="stack_question_list"),
-    url('^add/$', views.add, name="stack_question_add"),
-    url('^preview/$', views.preview, name="stack_question_preview"),
-    url('^(?P<slug>[^/]+)/$', views.detail, name="stack_question_detail"),
-    url('^(?P<slug>[^/]+)/accepted-answer/(?P<comment>[^/]+)/$', views.accepted_answer, name="stack_accepted_answer"),
+    re_path('^$', views.home, name="stack_question_home"),
+    re_path('^all/$', views.QuestionList.as_view(), name="stack_question_list"),
+    re_path('^add/$', views.add, name="stack_question_add"),
+    re_path('^preview/$', views.preview, name="stack_question_preview"),
+    re_path('^(?P<slug>[^/]+)/$', views.detail, name="stack_question_detail"),
+    re_path('^(?P<slug>[^/]+)/accepted-answer/(?P<comment>[^/]+)/$', views.accepted_answer, name="stack_accepted_answer"),
 ]
